f6145ecf2317ec326c3e5fc264282e1fa48d6f92
jghibiki <jghibiki.games@gmail.com>
2016-03-10 16:40:27 -0600

    Adds card images and icons

--- app/viewer/viewer.js
+++ app/viewer/viewer.js
@@ -2,9 +2,12 @@
 
 angular.module('magicBuddy.viewer', [angularDragula(angular)])
 
-.controller('ViewerCtrl', ["$scope", function($scope) {
+.controller('ViewerCtrl', ["$scope", "$sce", function($scope, $sce) {
 
     $scope.viewerCard = null;
+    $scope.symbolRe = /[^{}]+(?=\})/g;
+    $scope.viewerMode = "image";
+
     
     $scope.$on("viewer:showCard", function(e, card){
         $scope.viewerCard = card;
@@ -13,4 +16,42 @@ angular.module('magicBuddy.viewer', [angularDragula(angular)])
     $scope.$on("viewer:hideCard", function(e){
         $scope.viewerCard = null;
     })
+
+    $scope.manaSymbols = function(){
+        var symbols = [];
+
+        $scope.viewerCard.manaCost.match($scope.symbolRe).forEach(function(el){
+            symbols.push(el.toLowerCase());
+        });
+
+        return symbols
+    }
+
+    $scope.getUnderName = function(){
+        return encodeURI($scope.viewerCard.name.toLowerCase().replace(/ /g, "_").replace(/\'/g, "").replace(/-/g, "_") + ".jpg");
+    }
+
+    $scope.showImage = function(){
+        return $scope.viewerMode == "image";
+    }
+
+    $scope.showText = function(){
+        return $scope.viewerMode == "text";
+    }
+
+    $scope.showBoth = function(){
+        return $scope.viewerMode == "both";
+    }
+
+    $scope.cardText = function(){
+        var text = $scope.viewerCard.text;
+        text = text.replace($scope.symbolRe, function(x){
+            console.log(x);
+            return "<span class='mi mi-mana mi-" + x.toLowerCase() + "'></span>"
+        });
+        text = text.replace(/{/g, "").replace(/}/g, "");
+        return $sce.trustAsHtml(text);
+    }
+
+
 }]);
